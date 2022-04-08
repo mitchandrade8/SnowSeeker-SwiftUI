24ceb4f7ca5cd1df210a0c956004c1316b48b00b
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-04-08 05:24:59 -0600

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Mitch Andrade on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
