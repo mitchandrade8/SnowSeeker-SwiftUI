4d94631524b45b1520a7cb233388acb06e5e28c5
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-04-08 05:36:50 -0600

    first commit

---
+++ SnowSeeker/Practice/WorkingWithTwosidebysideViews.swift
@@ -0,0 +1,20 @@
+//
+//  WorkingWithTwosidebysideViews.swift
+//  SnowSeeker
+//
+//  Created by Mitch Andrade on 4/8/22.
+//
+
+import SwiftUI
+
+struct WorkingWithTwosidebysideViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WorkingWithTwosidebysideViews_Previews: PreviewProvider {
+    static var previews: some View {
+        WorkingWithTwosidebysideViews()
+    }
+}
