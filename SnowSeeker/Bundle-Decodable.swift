0479eb8e67f209a2b16c81a71552fc233432b01f
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-04-08 17:32:21 -0600

    third commit

---
+++ SnowSeeker/Bundle-Decodable.swift
@@ -0,0 +1,8 @@
+//
+//  Bundle-Decodable.swift
+//  SnowSeeker
+//
+//  Created by Mitch Andrade on 4/8/22.
+//
+
+import Foundation
---
+++ SnowSeeker/Practice/MakingASwiftUIViewSearchable.swift
@@ -0,0 +1,20 @@
+//
+//  MakingASwiftUIViewSearchable.swift
+//  SnowSeeker
+//
+//  Created by Mitch Andrade on 4/8/22.
+//
+
+import SwiftUI
+
+struct MakingASwiftUIViewSearchable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MakingASwiftUIViewSearchable_Previews: PreviewProvider {
+    static var previews: some View {
+        MakingASwiftUIViewSearchable()
+    }
+}
